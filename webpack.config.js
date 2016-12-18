ce94a9d2e6c5a901e0e66fc3b128a746eb29ad28
Travis Jefferson <traviscjefferson@gmail.com>
2016-12-18 17:30:43 -0600

    Good start on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'app.jsx'),
+  entry: [
+    path.resolve(__dirname, 'src', 'components', 'app')
+  ],
   output: {
     filename: 'bundle.js',
     path: './dist'
+  },
+  debug: true,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: path.resolve(__dirname, 'node_modules'),
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015']
-      }
+      loader: 'babel-loader'
     }]
   }
 };
