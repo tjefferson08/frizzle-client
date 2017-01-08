22b3191922c88592b7d991d73bf1a60e1b1c7132
Travis Jefferson <traviscjefferson@gmail.com>
2017-01-08 15:06:36 -0600

    Placeholder for new stop details card
    We'll want a list of incoming buses on this "page"

---
+++ src/components/presenters/StopDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StopDetails = ({ stop_id }) => {
+  return (
+    <div>
+      <h3>{stop_id}</h3>
+      <div>
+        I'm a details card.
+      </div>
+    </div>
+  );
+};
+
+export default StopDetails;
