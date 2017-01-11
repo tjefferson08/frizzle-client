1dc360eeec3698c5a5a5b315c89adbde02a7fdde
Travis Jefferson <traviscjefferson@gmail.com>
2017-01-10 21:44:31 -0600

    StopCard gets a link element

--- src/components/presenters/StopCard.jsx
+++ src/components/presenters/StopCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router'
+
 
 const StopCard = ({ stop_name, stop_id, stop_distance, route_names }) => {
   return (
     <li>
       <h3>{stop_name}</h3>
       <div>
-        <em>Id: {stop_id}</em>
+        <Link to={`/stops/${stop_id}`}>
+          <em>Id: {stop_id}</em>
+        </Link>
       </div>
       <div>
         Servicing routes: {(route_names || []).join(', ')}
